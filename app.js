133d88d771501410e8402d5a6d3fe8b2fb79feeb
Cedric Bien-Aime <bienaime009@gmail.com>
2026-08-03 16:33:29 -0400

    v1.0.9 - Optimizing app layout

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'englobe-app-v1.0.8';
+const APP_VERSION = 'englobe-app-v1.0.9';
 
 // ========================================== //
 // 1. NAVIGATION ET INTERFACE GLOBALE         //
