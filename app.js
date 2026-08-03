495fc963a999fb58abdea95ba7d682fb3c042247
Cedric Bien-Aime <bienaime009@gmail.com>
2026-08-03 18:01:52 -0400

    v1.0.9.5 - App layout

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'englobe-app-v1.0.9.4';
+const APP_VERSION = 'englobe-app-v1.0.9.5';
 
 // ========================================== //
 // 1. NAVIGATION ET INTERFACE GLOBALE         //
